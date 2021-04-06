dcc38a9d5e9ab62646933b4d4cbda6cbd1551b25
Malak Aldelahmawi <maldelah@umich.edu>
2021-04-06 15:33:01 -0400

    added teardown and setup methods

---
+++ src/test/java/org/apache/commons/mail/EmailConcrete.java
@@ -0,0 +1,12 @@
+package org.apache.commons.mail;
+import java.util.Map;
+
+public class EmailConcrete extends Email{
+
+	@Override
+	public Email setMsg(String msg) throws EmailException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,41 @@
+package org.apache.commons.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMultipart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sun.tools.javac.util.Assert;
+import com.sun.tools.javac.util.List;
+
+public class EmailTest {
+	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org", "abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
+	private static final Authenticator Authenticator = null;
+	private static final Session Session = null;
+	private String[] testValidChars= {" ", "a", "A", "\uc5ec", "0123456789", "12345678901234567890"};
+	
+	private EmailConcrete email;
+	
+	@Before //SETUP METHOD
+	public void setUpEmailTest() throws Exception{
+		email = new EmailConcrete(); //creating an instance
+		
+	}
+	
+	
+	@After //TEARDOWN METHOD
+	public void tearDownEmailTest() throws Exception{
+		
+	}
+}
